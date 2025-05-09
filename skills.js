54e344a4565693f32ef36ea06070c80613b373d5
Srivathsan G <77399329+srivat2001@users.noreply.github.com>
2025-05-09 18:57:11 +0530

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,2 +1,53 @@
-var data = '[{"nameofskill" : "Discord Bot", "link" : "https://discord.gg/g4a4jS8A","source":"https://replit.com/@bbbot2021/botbanerjee#main.py","imgofskill" : "https://beebom.com/wp-content/uploads/2018/02/discord-bots.jpg","desc" : "Bot with simple commands made using discord API and hosted using Flask", "langused" : "Python"},{"nameofskill" : "HVAC Website", "link" : "https://srivat2001.github.io/TreatedAer/#/index","source":"https://github.com/srivat2001/TreatedAer","imgofskill" : "https://i.ibb.co/g7sgVY3/Screenshot-2022-10-28-at-18-03-29-React-App.png","desc" : "Paid Website for HVAC vendor using React and Firebase", "langused" : "React"},{"nameofskill" : "SRM Canteen", "link" : "https://github.com/srivat2001/SRMCANTEEN","source":"https://github.com/srivat2001/SRMCANTEEN","imgofskill" : "img/srmcanteen.png","desc" : "3 Tier Food Takeaway Website for College Canteen", "langused" : "HTML, CSS, JS, SQL, PHP"},{"nameofskill" : "Pinnacle Website", "link" : "https://srivat2001.github.io/pinnacle2k22/","source":"https://github.com/srivat2001/pinnacle2k22","imgofskill" : "img/sympo.png","desc" : "Department Symposium Website (Frontend)", "langused" : "HTML, CSS, JS"},{"nameofskill" : "Club Website", "link" : "https://srivat2001.github.io/techsaavyclub/","source":"https://github.com/srivat2001/techsaavyclub","imgofskill" : "img/techsaavy.png","desc" : "Department Club Website", "langused" : "HTML, CSS, JS"}]';
-var smallprojects = '[{"nameofskill" : "EVM", "link" : "https://discord.gg/g4a4jS8A","source":"https://replit.com/@bbbot2021/botbanerjee#main.py","imgofskill" : "https://beebom.com/wp-content/uploads/2018/02/discord-bots.jpg","desc" : "An Simple EVM", "langused" : "JS(React)"}]';
+var data = '[\
+  {\
+    "nameofskill": "Satirical Blog",\
+    "link": "https://the-comibal-cabinet-dup.vercel.app/",\
+    "source": "https://github.com/srivat2001/theComibalCabinetDup",\
+    "imgofskill": "img/blog.png",\
+    "desc": "Personal satire blog with Google login and Firebase DB",\
+    "langused": "Next.js, Firebase"\
+  },\
+  {\
+    "nameofskill": "HVAC Website",\
+    "link": "https://srivat2001.github.io/TreatedAer/#/index",\
+    "source": "https://github.com/srivat2001/TreatedAer",\
+    "imgofskill": "https://i.ibb.co/g7sgVY3/Screenshot-2022-10-28-at-18-03-29-React-App.png",\
+    "desc": "Paid client project (2022) for HVAC vendor using React & Firebase",\
+    "langused": "React"\
+  },\
+  {\
+    "nameofskill": "SRM Canteen",\
+    "link": "https://github.com/srivat2001/SRMCANTEEN",\
+    "source": "https://github.com/srivat2001/SRMCANTEEN",\
+    "imgofskill": "img/srmcanteen.png",\
+    "desc": "3-tier food ordering system for college canteen (PHP + SQL)",\
+    "langused": "HTML, CSS, JS, PHP, SQL"\
+  },\
+  {\
+    "nameofskill": "Pinnacle Website",\
+    "link": "https://srivat2001.github.io/pinnacle2k22/",\
+    "source": "https://github.com/srivat2001/pinnacle2k22",\
+    "imgofskill": "img/sympo.png",\
+    "desc": "Department symposium website (frontend only)",\
+    "langused": "HTML, CSS, JS"\
+  },\
+  {\
+    "nameofskill": "Club Website",\
+    "link": "https://srivat2001.github.io/techsaavyclub/",\
+    "source": "https://github.com/srivat2001/techsaavyclub",\
+    "imgofskill": "img/techsaavy.png",\
+    "desc": "Official department club site made with basic stack",\
+    "langused": "HTML, CSS, JS"\
+  }\
+]';
+
+var smallprojects = '[\
+  {\
+    "nameofskill": "EVM",\
+    "link": "https://discord.gg/g4a4jS8A",\
+    "source": "https://replit.com/@bbbot2021/botbanerjee#main.py",\
+    "imgofskill": "https://beebom.com/wp-content/uploads/2018/02/discord-bots.jpg",\
+    "desc": "Simple EVM interface mockup using basic React",\
+    "langused": "React"\
+  }\
+]';
